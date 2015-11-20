1875eb04c7fa8d7f4b9d49504385077e72d45794
Joe Buhlig <joe@joebuhlig.com>
2015-11-20 13:58:59 -0600

    Changed deface hooks

--- app/overrides/add_ad_hoc_option_types_to_cart_form.rb
+++ app/overrides/add_ad_hoc_option_types_to_cart_form.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/products/_cart_form",
                      :name => "converted_product_price_733808074",
-                     :insert_before => "[data-hook='product_price'], #product_price[data-hook]",
+                     :replace => "[data-hook='ad_hoc_options']",
                      :partial => "spree/products/ad_hoc_option_types")

5d877e25e1dc78120543e0b24ee1a9cf65b2b04b
Joe Buhlig <joe@joebuhlig.com>
2015-11-20 13:06:13 -0600

    Added missing parameters

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -45,14 +45,14 @@ module Spree
       line_item
       end
   
-      def grab_line_item_by_variant(variant, raise_error = false, ad_hoc_option_value_ids = [], product_customizations = [])
+      def grab_line_item_by_variant(variant, raise_error = false, options = {}, ad_hoc_option_value_ids, product_customizations)
         line_item = order.find_line_item_by_variant(variant, options)
 
         if !line_item.present? && raise_error
           raise ActiveRecord::RecordNotFound, "Line item not found for variant #{variant.sku}"
         end
 
-      line_item
-      end
+        line_item
+    end
   end
 end

28947e3c1febbec4afa683f512b2060b516dd67d
Joe Buhlig <joe@joebuhlig.com>
2015-11-23 06:39:38 -0600

    Revert populate

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -10,30 +10,30 @@ module Spree
     # needs to occur in the cart partial for this to be done 'right'
     #
     # Adds a new item to the order (creating a new order if none already exists)
-    def populate
-      order    = current_order(create_order_if_necessary: true)
-      variant  = Spree::Variant.find(params[:variant_id])
-      quantity = params[:quantity].to_i
-      options  = params[:options] || {}
-      # 2,147,483,647 is crazy. See issue #2695.
-      if quantity.between?(1, 2_147_483_647)
-        begin
-          order.contents.add(variant, quantity, options, ad_hoc_option_value_ids, product_customizations)
-        rescue ActiveRecord::RecordInvalid => e
-          error = e.record.errors.full_messages.join(", ")
-        end
-      else
-        error = Spree.t(:please_enter_reasonable_quantity)
-      end
+    # def populate
+    #   order    = current_order(create_order_if_necessary: true)
+    #   variant  = Spree::Variant.find(params[:variant_id])
+    #   quantity = params[:quantity].to_i
+    #   options  = params[:options] || {}
+    #   # 2,147,483,647 is crazy. See issue #2695.
+    #   if quantity.between?(1, 2_147_483_647)
+    #     begin
+    #       order.contents.add(variant, quantity, options, ad_hoc_option_value_ids, product_customizations)
+    #     rescue ActiveRecord::RecordInvalid => e
+    #       error = e.record.errors.full_messages.join(", ")
+    #     end
+    #   else
+    #     error = Spree.t(:please_enter_reasonable_quantity)
+    #   end
 
-      if error
-        flash[:error] = error
-        redirect_back_or_default(spree.root_path)
-      else
-        respond_with(order) do |format|
-          format.html { redirect_to cart_path }
-        end
-      end
-    end
+    #   if error
+    #     flash[:error] = error
+    #     redirect_back_or_default(spree.root_path)
+    #   else
+    #     respond_with(order) do |format|
+    #       format.html { redirect_to cart_path }
+    #     end
+    #   end
+    # end
   end
 end
